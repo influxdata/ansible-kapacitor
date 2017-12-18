69a11a4e8f45a71b8b4a0681c49934d948117bac
Paul Myjavec <pauly@myjavec.com>
2017-12-18 17:54:53 +1100

    TICK Template support
    Adds support for managing TICK task templates as well as defining tasks
    using said templates.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -6,3 +6,41 @@ describe service('kapacitor') do
   it { should be_running }
   it { should be_enabled }
 end
+
+describe service('kapacitor') do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+describe file('/usr/local/etc/kapacitor/tick') {
+  it { should be_directory }
+}
+
+describe file('/usr/local/etc/kapacitor/tick/script') {
+  it { should be_directory }
+}
+
+describe file('/usr/local/etc/kapacitor/tick/template') {
+  it { should be_directory }
+}
+
+describe file('/usr/local/etc/kapacitor/tick/var') {
+  it { should be_directory }
+}
+
+describe file('/usr/local/etc/kapacitor/tick/template/example.tick') do
+  it { should be_file }
+end
+
+describe file('/usr/local/etc/kapacitor/tick/var/example.json') do
+  it { should be_file }
+end
+
+describe command('kapacitor list templates') do
+  its(:stdout) { should match /example.*stream/ }
+end
+
+describe command('kapacitor list tasks') do
+  its(:stdout) { should match /example.*disabled.*false/ }
+  its(:stdout) { should match /template_test.*enabled.*true/ }
+end
